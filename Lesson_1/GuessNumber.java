d836d4aa000a870b46128646909f1732976f93e1
Kirill Dukhov <qkidle@gmail.com>
2022-03-29 20:56:41 +0700

    Homework GuessNumber

---
+++ Lesson_1/GuessNumber.java
@@ -0,0 +1,19 @@
+public class GuessNumber {
+    public static void main(String[] args) {
+        int playerNum = 10;
+        int hiddenNum = 5;
+
+        for (int i = 1; i <= 100; i++){
+            if (hiddenNum > playerNum) {
+                System.out.println("Данное число больше того, что загадал компьютер");
+                hiddenNum--;
+            } else if (hiddenNum < playerNum) {
+                System.out.println("Данное число меньше того, что загадал компьютер");
+                hiddenNum++;
+            } else {
+                System.out.println("Вы победили!");
+                break;
+            }
+        }
+    }
+}
