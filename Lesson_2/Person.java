1b3946b2624c406c25c3b52c063ba5f6fe407a24
Kirill Dukhov <qkidle@gmail.com>
2022-04-04 20:38:13 +0700

    push lesson 2,homework one

---
+++ Lesson_2/Person.java
@@ -0,0 +1,31 @@
+public class Person {
+
+    String gender = "Male";
+    String name = "Kirill"ж
+    float height = 1.89f;
+    float weight = 87f;
+    int age = 24;
+
+    void go() {
+        System.out.println("Идти");
+    }
+
+    void sit() {
+        System.out.println("Сидит");
+    }
+
+    boolean run() {
+        System.out.println("Бежит");
+        return true;
+    }
+
+    boolean talk() {
+        System.out.println("Говорит");
+        return true;
+    }
+
+    boolean learnJava() {
+        System.out.println("Учит Java");
+        return true;
+    }
+}
