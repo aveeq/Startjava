864a9b34255fd1f38424b2768c520bd77c1e543d
Kirill Dukhov <qkidle@gmail.com>
2022-04-05 21:20:30 +0700

    Home work Wolf and wolfTest

---
+++ Lesson_2/WolfTest.java
@@ -0,0 +1,21 @@
+public class WolfTest {
+
+    String nickname;
+    float weight;
+    int age;
+    String color;
+
+    public static void main(String[] args) {
+        WolfTest wolf = new WolfTest();
+        wolf.nickname = "Кузя";
+        wolf.weight = 10.3f;
+        wolf.age = 12;
+        wolf.color = "Серый";
+
+        System.out.println("Кличка Волка: " + wolf.nickname);
+        System.out.println("Вес: " + wolf.weight);
+        System.out.println("Возраст: " + wolf.age);
+        System.out.println("Цвет: " + wolf.color);
+    }
+
+}
