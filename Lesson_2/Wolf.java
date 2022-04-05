33d264e124e47d259383674de1b92241b1f6fe3a
Kirill Dukhov <qkidle@gmail.com>
2022-04-05 21:19:55 +0700

    Home work Wolf and wolfTest

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,28 @@
+public class Wolf {
+
+    String gender;
+    String nickname;
+    float weight;
+    int age;
+    String color;
+
+    void go() {
+        System.out.println("Идти");
+    }
+
+    void sit() {
+        System.out.println("Сидит");
+    }
+
+    void run() {
+        System.out.println("Бежит");
+    }
+
+    void how() {
+        System.out.println("Воет");
+    }
+
+    void hunt() {
+        System.out.println("Охотиться");
+    }
+}
