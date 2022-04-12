4c0c9c1637cdf01e1e9bdffae02410b8f3990e2d
Kirill Dukhov <qkidle@gmail.com>
2022-04-12 19:14:24 +0700

    Home work Jaeger and JaegerTest

---
+++ Lesson_2/Jaeger.java
@@ -0,0 +1,75 @@
+public class Jaeger {
+
+    private String modelName;
+    private String markNumber;
+    private String origin;
+    private float height;
+    private float weight;
+    private int speed;
+    private int strength;
+    private int armor;
+    
+    public String getModelName() {
+        return modelName;
+    }
+    
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+    
+    public String getMarkNumber() {
+        return markNumber;
+    }
+    
+    public void setMarkNumber(String markNumber) {
+        this.markNumber = markNumber;
+    }
+    
+    public String getOrigin() {
+        return origin;
+    }
+    
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+    
+    public float getHeight() {
+        return height;
+    }
+    
+    public void setHeight(float height) {
+        this.height = height;
+    }
+    
+    public float getWeight() {
+        return weight;
+    }
+    
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+    
+    public int getSpeed() {
+        return speed;
+    }
+    
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    
+    public int getStrength() {
+        return strength;
+    }
+    
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+    
+    public int getArmor() {
+        return armor;
+    }
+    
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+}
